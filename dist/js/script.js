293bfe776cab5cbc729cf6423c530de92a8d3d73
Andrii <programsweb2022@gmail.com>
2023-04-12 19:33:20 +0300

    section program 1 part

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,33 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("program__accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     } 
   });
 }
+
+/* (function ($) {
+  $(document).ready(function () {
+      $('.accordeon-label').each(function () {
+          if ($(this).hasClass('opened')) {
+              $('.accordeon-content', this).show();
+          }
+      });
+  });
+
+  $('.accordeon-label').on('click', function () {
+      $(this).toggleClass('opened');
+      $('.accordeon-content', this).toggle(300);
+      if ($(this).hasClass('opened')) {
+          $(this).siblings().removeClass('opened');
+          $(this).siblings().children('.accordeon-content').hide(300);
+      }
+  });
+})(jQuery); */
