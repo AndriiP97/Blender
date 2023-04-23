e6432a85064d0853fd9af2e5ab481e77cb42eb11
Andrii <programsweb2022@gmail.com>
2023-04-23 15:40:46 +0300

    script record - form and window thanks part 1

--- src/js/script.js
+++ src/js/script.js
@@ -13,21 +13,69 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-/* (function ($) {
-  $(document).ready(function () {
-      $('.accordeon-label').each(function () {
-          if ($(this).hasClass('opened')) {
-              $('.accordeon-content', this).show();
-          }
-      });
+$(document).ready(function(){
+  $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+      $('.overlay, #thanks').fadeOut('slow');
   });
 
-  $('.accordeon-label').on('click', function () {
-      $(this).toggleClass('opened');
-      $('.accordeon-content', this).toggle(300);
-      if ($(this).hasClass('opened')) {
-          $(this).siblings().removeClass('opened');
-          $(this).siblings().children('.accordeon-content').hide(300);
-      }
-  });
-})(jQuery); */
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            checkbox: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            checkbox: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+validateForms('#access-form');
+validateForms('#consultation form');
+
+$('input[name=phone]').mask("+7 (999) 999-9999");
+
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+
+
+});
